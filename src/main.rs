37eb5e49cdecac73508021b3f6810ce4b8ad7338
hippietrail <hippytrail@gmail.com>
2026-07-23 19:17:59 +0700

    refactor: into functions and basic structures

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,43 @@
+// 1. local modules
 mod pos;
 
-use reqwest;
-use serde_json;
-
+// 2. stdlib
 use std::{
     collections::{HashMap, HashSet},
     env,
 };
 
+// 3. external crates
 use harper_core::spell::{Dictionary, FstDictionary};
+use serde::Deserialize;
 
-use pos::POS;
+// 4. local modules
+use pos::Pos;
 
 pub struct Cfg {
-    // pub debug: bool,
-    // pub verbose: bool,
+    pub raw: bool,
     pub alternatives: Vec<String>,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn cli() -> Cfg {
     let mut args: Vec<String> = env::args().skip(1).collect();
     let mut cfg = Cfg {
-        // debug: false,
-        // verbose: false,
+        raw: false,
         alternatives: Vec::new(),
     };
 
     while let Some(arg) = args.pop() {
-        // if arg == "--debug" {
-        //     cfg.debug = true;
-        // } else if arg == "--verbose" {
-        //     cfg.verbose = true;
-        // } else {
-        cfg.alternatives.push(arg);
-        // }
+        if arg == "--raw" {
+            cfg.raw = true;
+        } else {
+            cfg.alternatives.push(arg);
+        }
     }
 
+    cfg
+}
+
+fn build_url(cfg: &Cfg) -> url::Url {
     let mut url = url::Url::parse("https://books.google.com/ngrams/json").unwrap();
 
     // comma-separated list of terms with wildcards
@@ -48,181 +50,176 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     url.query_pairs_mut().append_pair("content", &content);
 
-    let mut graph_url = url.clone();
-    graph_url.path_segments_mut().unwrap().pop().push("graph");
-    println!("ℹ️ URL: {}", graph_url);
+    url
+}
 
+fn fetch_json(url: url::Url) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
     let response = reqwest::blocking::get(url)?;
     let json = response.json::<serde_json::Value>()?;
+    Ok(json)
+}
 
-    let mut variant_to_pre_and_post: HashMap<String, [Vec<String>; 2]> = HashMap::new();
+#[derive(Deserialize)]
+#[serde(rename_all = "lowercase")]
+struct NgramItem {
+    ngram: String,
+    parent: String,
+    #[serde(rename = "type")]
+    kind: NgramType,
+    // We may use this later with clustering to pay only attention to the highest frequency
+    #[serde(rename = "timeseries")]
+    _timeseries: serde_json::Value,
+}
 
-    let known_keys = ["ngram", "parent", "timeseries", "type"];
-    for ele in json.as_array().unwrap() {
-        let ob = ele.as_object().unwrap();
+#[derive(Deserialize, PartialEq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+enum NgramType {
+    NgramCollection,
+    Expansion,
+    Ngram,
+}
 
-        for key in ob.keys() {
-            if !known_keys.contains(&key.as_str()) {
-                return Err(format!("Unknown key: {}", key).into());
+// A 'variant' is a word or phrase we're comparing against other variants: "their" vs "there" etc.
+// A 'context' is a single word that appears before or after a variant: [their] "own" or [there] "is"
+type VariantContextMap = HashMap<String, [Vec<String>; 2]>;
+
+fn parse(json: serde_json::Value) -> Result<VariantContextMap, Box<dyn std::error::Error>> {
+    let items: Vec<NgramItem> = serde_json::from_value(json)?;
+    let mut variant_to_pre_and_post = VariantContextMap::new();
+
+    for item in items {
+        if item.kind != NgramType::Expansion {
+            continue;
+        }
+
+        let (index, variant, context_word) = match item.parent.as_str() {
+            p if p.starts_with("* ") => {
+                let v = &p[2..];
+                let c = item
+                    .ngram
+                    .strip_suffix(&format!(" {}", v))
+                    .unwrap_or(&item.ngram);
+                (0, v.to_string(), c.to_string())
             }
-            if key == "type" {
-                let type_value = ob.get(key).unwrap();
-                if type_value != "NGRAM_COLLECTION"
-                    && type_value != "EXPANSION"
-                    && type_value != "NGRAM"
-                {
-                    return Err(format!("Unknown type: {}", type_value).into());
-                }
+            p if p.ends_with(" *") => {
+                let v = &p[..p.len() - 2];
+                let c = item
+                    .ngram
+                    .strip_prefix(&format!("{} ", v))
+                    .unwrap_or(&item.ngram);
+                (1, v.to_string(), c.to_string())
             }
-        }
+            _ => {
+                return Err(format!(
+                    "Parent '{}' must start with '* ' or end with ' *'",
+                    item.parent
+                )
+                .into());
+            }
+        };
 
-        let (ngram, parent, _timeseries, kind) = (
-            ob.get("ngram").unwrap().as_str().unwrap(),
-            ob.get("parent").unwrap().as_str().unwrap(),
-            ob.get("timeseries").unwrap(),
-            ob.get("type").unwrap().as_str().unwrap(),
-        );
-
-        enum Which {
-            Start,
-            End,
-        }
-
-        if kind == "EXPANSION" {
-            let (wh, index) = match parent {
-                p if p.starts_with("* ") => (Which::Start, 0),
-                p if p.ends_with(" *") => (Which::End, 1),
-                _ => {
-                    return Err(format!(
-                        "Parent {} does not start with '* ' or end with ' *'",
-                        parent
-                    )
-                    .into());
-                }
-            };
-
-            let (variant, context_word) = match wh {
-                Which::Start => {
-                    // parent is like "* to"
-                    let v = parent.strip_prefix("* ").unwrap().to_string();
-                    // ngram is like "according to" -> we want "according"
-                    let c = ngram
-                        .strip_suffix(&format!(" {}", v))
-                        .unwrap_or(ngram) // Fallback safety
-                        .to_string();
-                    (v, c)
-                }
-                Which::End => {
-                    // parent is like "to *"
-                    let v = parent.strip_suffix(" *").unwrap().to_string();
-                    // ngram is like "to make" -> we want "make"
-                    let c = ngram
-                        .strip_prefix(&format!("{} ", v))
-                        .unwrap_or(ngram)
-                        .to_string();
-                    (v, c)
-                }
-            };
-
-            variant_to_pre_and_post.entry(variant).or_default()[index].push(context_word);
-        }
+        variant_to_pre_and_post.entry(variant).or_default()[index].push(context_word);
     }
 
-    // for each variant, for each kind of context (pre, post), find which are the 'discriminators'
-    // i.e. the ones in its set but not in the equivalent set of the other variants
+    Ok(variant_to_pre_and_post)
+}
 
-    // a way to do this is to make a map from each context word to the variants that use it
-    // then for each variant, for each kind of context, find the words that only appear in that variant's contexts
+// The variants become the values, since we're looking for contexts that are unique to each variant
+pub struct Analysis {
+    pub pre_poses: HashMap<&'static Pos, HashSet<String>>,
+    pub post_poses: HashMap<&'static Pos, HashSet<String>>,
+    pub pre_words_case: HashMap<String, Vec<String>>,
+    pub post_words_case: HashMap<String, Vec<String>>,
+    pub pre_words_nocase: HashMap<String, Vec<String>>,
+    pub post_words_nocase: HashMap<String, Vec<String>>,
+}
 
+// Look up part-of-speech tags for a word using Harper's curated dictionary
+fn get_poses(dict: &FstDictionary, word: &str) -> Vec<&'static Pos> {
+    dict.get_word_metadata_str(word)
+        .map_or_else(std::vec::Vec::new, |md| {
+            pos::POS_DEFINITIONS
+                .iter()
+                .filter(|&(_, pred)| pred(&md))
+                .map(|(enum_variant, _)| enum_variant)
+                .collect()
+        })
+}
+
+fn analyse(
+    variant_to_pre_and_post: VariantContextMap,
+) -> Result<Analysis, Box<dyn std::error::Error>> {
     let dict = FstDictionary::curated();
 
-    let get_poses = |word: &str| -> Vec<&POS> {
-        dict.get_word_metadata_str(word).map_or_else(
-            || vec![],
-            |md| {
-                pos::POS_DEFINITIONS
-                    .iter()
-                    .filter(|&(_, pred)| pred(&md))
-                    .map(|(enum_variant, _)| enum_variant)
-                    .collect()
-            },
-        )
+    let mut res = Analysis {
+        pre_poses: HashMap::new(),
+        post_poses: HashMap::new(),
+        pre_words_case: HashMap::new(),
+        post_words_case: HashMap::new(),
+        pre_words_nocase: HashMap::new(),
+        post_words_nocase: HashMap::new(),
     };
 
-    let mut pre_words_case: HashMap<String, Vec<String>> = HashMap::new();
-    let mut post_words_case: HashMap<String, Vec<String>> = HashMap::new();
+    for (variant, [pre_words, post_words]) in variant_to_pre_and_post {
+        for word in pre_words {
+            res.pre_words_case
+                .entry(word.clone())
+                .or_default()
+                .push(variant.clone());
 
-    let mut pre_words_nocase: HashMap<String, Vec<String>> = HashMap::new();
-    let mut post_words_nocase: HashMap<String, Vec<String>> = HashMap::new();
+            res.pre_words_nocase
+                .entry(word.to_lowercase())
+                .or_default()
+                .push(variant.clone());
 
-    let mut pre_poses: HashMap<&POS, HashSet<String>> = HashMap::new();
-    let mut post_poses: HashMap<&POS, HashSet<String>> = HashMap::new();
-
-    let context_kind_names = ["pre", "post"];
-
-    for (variant, contexts) in variant_to_pre_and_post.iter() {
-        for (i, context_words_case) in contexts.iter().enumerate() {
-            let context_kind = context_kind_names[i];
-            let (ctx_to_case, ctx_to_nocase) = match context_kind {
-                "pre" => (&mut pre_words_case, &mut pre_words_nocase),
-                "post" => (&mut post_words_case, &mut post_words_nocase),
-                _ => panic!("Invalid context kind name: {}", context_kind),
-            };
-
-            for context_word_case in context_words_case {
-                ctx_to_case
-                    .entry(context_word_case.clone())
+            for pos in get_poses(&dict, &word) {
+                res.pre_poses
+                    .entry(pos)
                     .or_default()
-                    .push(variant.clone());
+                    .insert(variant.clone());
+            }
+        }
 
-                ctx_to_nocase
-                    .entry(context_word_case.to_lowercase())
+        for word in post_words {
+            res.post_words_case
+                .entry(word.clone())
+                .or_default()
+                .push(variant.clone());
+
+            res.post_words_nocase
+                .entry(word.to_lowercase())
+                .or_default()
+                .push(variant.clone());
+
+            for pos in get_poses(&dict, &word) {
+                res.post_poses
+                    .entry(pos)
                     .or_default()
-                    .push(variant.clone());
-
-                for pos in get_poses(context_word_case) {
-                    match context_kind {
-                        "pre" => {
-                            pre_poses.entry(pos).or_default().insert(variant.clone());
-                        }
-                        "post" => {
-                            post_poses.entry(pos).or_default().insert(variant.clone());
-                        }
-                        _ => panic!("Invalid context kind name: {}", context_kind),
-                    }
-                }
+                    .insert(variant.clone());
             }
         }
     }
 
-    output(
-        cfg,
+    Ok(res)
+}
+
+fn output(cfg: Cfg, analysis: Analysis) {
+    let Analysis {
         pre_poses,
         post_poses,
         pre_words_case,
         post_words_case,
         pre_words_nocase,
         post_words_nocase,
-    );
+    } = analysis;
 
-    Ok(())
-}
-
-fn output(
-    cfg: Cfg,
-    pre_poses: HashMap<&POS, HashSet<String>>,
-    post_poses: HashMap<&POS, HashSet<String>>,
-    pre_words_case: HashMap<String, Vec<String>>,
-    post_words_case: HashMap<String, Vec<String>>,
-    pre_words_nocase: HashMap<String, Vec<String>>,
-    post_words_nocase: HashMap<String, Vec<String>>,
-) {
     for variant in &cfg.alternatives {
         let pre_pos = pre_poses
             .iter()
             .filter(|(_, poses)| poses.len() == 1 && poses.contains(variant))
             .map(|(pos, _)| pos)
             .collect::<Vec<_>>();
+
         let post_pos = post_poses
             .iter()
             .filter(|(_, poses)| poses.len() == 1 && poses.contains(variant))
@@ -231,29 +228,31 @@ fn output(
 
         let mut pre_words = pre_words_case
             .iter()
-            .filter(|(_, variants)| variants.len() == 1 && variants.contains(&variant))
+            .filter(|(_, variants)| variants.len() == 1 && variants.contains(variant))
             .map(|(word, _)| word.as_str())
             .collect::<Vec<_>>();
-        pre_words.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+        pre_words.sort_by_key(|a| a.to_lowercase());
+
         let mut post_words = post_words_case
             .iter()
-            .filter(|(_, variants)| variants.len() == 1 && variants.contains(&variant))
+            .filter(|(_, variants)| variants.len() == 1 && variants.contains(variant))
             .map(|(word, _)| word.as_str())
             .collect::<Vec<_>>();
-        post_words.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+        post_words.sort_by_key(|a| a.to_lowercase());
 
         let mut pre_words_nocase = pre_words_nocase
             .iter()
-            .filter(|(_, variants)| variants.len() == 1 && variants.contains(&variant))
+            .filter(|(_, variants)| variants.len() == 1 && variants.contains(variant))
             .map(|(word, _)| word.as_str())
             .collect::<Vec<_>>();
-        pre_words_nocase.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+        pre_words_nocase.sort_by_key(|a| a.to_lowercase());
+
         let mut post_words_nocase = post_words_nocase
             .iter()
-            .filter(|(_, variants)| variants.len() == 1 && variants.contains(&variant))
+            .filter(|(_, variants)| variants.len() == 1 && variants.contains(variant))
             .map(|(word, _)| word.as_str())
             .collect::<Vec<_>>();
-        post_words_nocase.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+        post_words_nocase.sort_by_key(|a| a.to_lowercase());
 
         // POS and case-sensitive word discriminators
         println!(
@@ -287,11 +286,11 @@ fn output(
             let mut negative_pre_words = pre_words_case
                 .iter()
                 .filter(|(_, variants)| {
-                    variants.len() == cfg.alternatives.len() - 1 && !variants.contains(&variant)
+                    variants.len() == cfg.alternatives.len() - 1 && !variants.contains(variant)
                 })
                 .map(|(word, _)| word.as_str())
                 .collect::<Vec<_>>();
-            negative_pre_words.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+            negative_pre_words.sort_by_key(|a| a.to_lowercase());
 
             let mut negative_post_words = post_words_case
                 .iter()
@@ -300,7 +299,7 @@ fn output(
                 })
                 .map(|(word, _)| word.as_str())
                 .collect::<Vec<_>>();
-            negative_post_words.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+            negative_post_words.sort_by_key(|a| a.to_lowercase());
 
             println!(
                 "🚫 \x1b[31m {} | \x1b[33m{} \x1b[31m| {} \x1b[0m",
@@ -310,4 +309,20 @@ fn output(
             );
         }
     }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cfg = cli();
+    let url = build_url(&cfg);
+    let mut graph_url = url.clone();
+    graph_url.path_segments_mut().unwrap().pop().push("graph");
+    println!("ℹ️ URL: {}", graph_url);
+
+    let variant_to_pre_and_post = parse(fetch_json(url)?)?;
+
+    let analysis = analyse(variant_to_pre_and_post)?;
+
+    output(cfg, analysis);
+
+    Ok(())
 }
--- src/pos.rs
+++ src/pos.rs
@@ -3,7 +3,7 @@ use harper_core::DictWordMetadata;
 pub type PosPredicate = fn(&DictWordMetadata) -> bool;
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-pub enum POS {
+pub enum Pos {
     Adjective,
     Adverb,
     Conjunction,
@@ -22,69 +22,69 @@ pub struct POSInfo {
     pub _emoji: &'static str,
 }
 
-pub const POS_DEFINITIONS: &[(POS, PosPredicate)] = &[
-    (POS::Noun, |m| m.is_noun() && !m.is_proper_noun()),
-    (POS::ProperNoun, DictWordMetadata::is_proper_noun),
-    (POS::Verb, DictWordMetadata::is_verb),
-    (POS::Adjective, DictWordMetadata::is_adjective),
-    (POS::Adverb, DictWordMetadata::is_adverb),
-    (POS::Conjunction, DictWordMetadata::is_conjunction),
-    (POS::Determiner, DictWordMetadata::is_determiner),
-    (POS::Preposition, |m| m.preposition),
-    (POS::Pronoun, DictWordMetadata::is_pronoun),
+pub const POS_DEFINITIONS: &[(Pos, PosPredicate)] = &[
+    (Pos::Noun, |m| m.is_noun() && !m.is_proper_noun()),
+    (Pos::ProperNoun, DictWordMetadata::is_proper_noun),
+    (Pos::Verb, DictWordMetadata::is_verb),
+    (Pos::Adjective, DictWordMetadata::is_adjective),
+    (Pos::Adverb, DictWordMetadata::is_adverb),
+    (Pos::Conjunction, DictWordMetadata::is_conjunction),
+    (Pos::Determiner, DictWordMetadata::is_determiner),
+    (Pos::Preposition, |m| m.preposition),
+    (Pos::Pronoun, DictWordMetadata::is_pronoun),
 ];
 
-pub fn pos_info(pos: &POS) -> POSInfo {
+pub fn pos_info(pos: &Pos) -> POSInfo {
     match pos {
-        POS::Noun => POSInfo {
+        Pos::Noun => POSInfo {
             letter: "N",
             _ptb: "NN",
             _emoji: "📦",
             _name: "noun",
         },
-        POS::ProperNoun => POSInfo {
+        Pos::ProperNoun => POSInfo {
             letter: "O",
             _ptb: "NNP",
             _emoji: "📛",
             _name: "proper noun",
         },
-        POS::Verb => POSInfo {
+        Pos::Verb => POSInfo {
             letter: "V",
             _ptb: "VB",
             _emoji: "🏃",
             _name: "verb",
         },
-        POS::Adjective => POSInfo {
+        Pos::Adjective => POSInfo {
             letter: "J",
             _ptb: "JJ",
             _emoji: "🌈",
             _name: "adjective",
         },
-        POS::Adverb => POSInfo {
+        Pos::Adverb => POSInfo {
             letter: "R",
             _ptb: "RB",
             _emoji: "🤷",
             _name: "adverb",
         },
-        POS::Conjunction => POSInfo {
+        Pos::Conjunction => POSInfo {
             letter: "C",
             _ptb: "CC",
             _emoji: "🔗",
             _name: "conjunction",
         },
-        POS::Determiner => POSInfo {
+        Pos::Determiner => POSInfo {
             letter: "D",
             _ptb: "DT",
             _emoji: "👉",
             _name: "determiner",
         },
-        POS::Preposition => POSInfo {
+        Pos::Preposition => POSInfo {
             letter: "P",
             _ptb: "IN",
             _emoji: "📥",
             _name: "preposition",
         },
-        POS::Pronoun => POSInfo {
+        Pos::Pronoun => POSInfo {
             letter: "I",
             _ptb: "PRP",
             _emoji: "👤",
